c7518ef585dcc79508d742ea585800fe17faebd5
Jhordan_109 <134504952+jhwrdan109@users.noreply.github.com>
2024-01-11 20:32:17 -0400

    Add files via upload

---
+++ ProjectQuiz/script.js
@@ -0,0 +1,80 @@
+//Inicial Data
+let currentQuestion = 0
+let correctAnswers = 0
+ showQuestion()
+// Events
+
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent)
+
+ //functions
+
+
+ function showQuestion(){
+    if(questions[currentQuestion]){
+let q = questions[currentQuestion] 
+
+let pct = Math.floor((currentQuestion / questions.length)*100)
+
+document.querySelector('.progress--bar').style.width = `${pct}%`
+
+
+document.querySelector('.scoreArea').style.display = 'none'
+document.querySelector('.questionArea').style.display = 'block'
+
+document.querySelector('.question').innerHTML = q.question
+document.querySelector('.options').innerHTML = ''
+
+let optionsHtml = ''
+for(let i in q.options){
+    optionsHtml += `<div data-op="${i}"  class='option'><span>${parseInt(i)+1}</span>${q.options[i]}</div>`
+}
+
+document.querySelector('.options').innerHTML = optionsHtml
+
+
+document.querySelectorAll('.options .option').forEach(item =>{
+    item.addEventListener('click', optionClickEvent)
+})
+    }else{
+finishQuiz()
+    }
+}
+
+function optionClickEvent(e){
+let clickedOption = parseInt(e.target.getAttribute('data-op'))
+if(questions[currentQuestion].answer === clickedOption){
+correctAnswers++;
+}
+currentQuestion++
+showQuestion()  
+}
+
+function finishQuiz(){
+    let points = Math.floor((correctAnswers / questions.length) * 100)
+if(points < 30){
+    document.querySelector('.scoreText1').innerHTML = 'Tá ruim hein'
+    document.querySelector('.scorePct').style.color = '#FF0000'
+}else if(points >= 30 && points < 70){
+    document.querySelector('.scoreText1').innerHTML = 'Na média'
+    document.querySelector('.scorePct').style.color = '#FFF000'
+}else if( points >= 70 && points < 100){
+    document.querySelector('.scoreText1').innerHTML = 'Muleke é bom'
+    document.querySelector('.scorePct').style.color = '#0D630D'
+}else if(points = 100){
+    document.querySelector('.scoreText1').innerHTML = 'Gênio'
+    document.querySelector('.scorePct').style.color = 'white'
+}
+
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers} `
+    document.querySelector('.scoreArea').style.display = 'block'
+document.querySelector('.questionArea').style.display = 'none'
+document.querySelector('.progress--bar').style.width = '100%'
+
+}
+function resetEvent(){
+currentQuestion = 0
+showQuestion()
+    correctAnswers = 0
+}
